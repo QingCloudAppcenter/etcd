1d7e92955d57cc4de68401ba2ce25ed5c4f8657e
樊尚享 <martinfan@yunify.com>
2017-09-25 20:20:58 +0800

    fix defects in node addition

--- roles/etcd-service/files/script/join.sh
+++ roles/etcd-service/files/script/join.sh
@@ -8,10 +8,11 @@ export etcd_endpoint=$( ETCDCTL_API=3 etcdctl endpoint health | grep -v "unhealt
 lid=$(ETCDCTL_API=3 etcdctl -w fields lease grant 60 | grep '"ID"' | awk ' { print $3 } ')
 echo lockid=$(curl $etcd_endpoint/v3alpha/lock/lock -XPOST -d"{\"name\":\"ZXRjZGNsdXN0ZXJsb2Nr\", \"lease\" : $lid }"|jq '.key' ) >>  /etc/etcd/runtime_etcd.conf
 
-ETCDCTL_API=3 etcdctl member list --endpoints=$ETCDCTL_ENDPOINTS |grep started | awk -F \,  '{ gsub(/ /, "", $0);print $3"="$4 }'| while read node; do  if [[ -z ETCD_INITIAL_CLUSTER ]]; then export ETCD_INITIAL_CLUSTER=$node; else export ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,$node; fi; done
+unset ETCD_INITIAL_CLUSTER
+ETCDCTL_API=3 etcdctl member list --endpoints=$ETCDCTL_ENDPOINTS |grep ' started' | awk -F \,  '{ gsub(/ /, "", $0);print $3"="$4 }'|{  while read node; do  if [[ -z $ETCD_INITIAL_CLUSTER ]]; then export ETCD_INITIAL_CLUSTER=$node; else export ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,$node; fi; done; echo ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,etcd$UNIQUESID=$ETCD_INITIAL_ADVERTISE_PEER_URLS >>  /etc/etcd/runtime_etcd.conf ; }
 
-echo ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,etcd$UNIQUESID=$ETCD_INITIAL_ADVERTISE_PEER_URLS >>  /etc/etcd/runtime_etcd.conf
+ETCDCTL_API=3 etcdctl member add etcd$UNIQUESID --peer-urls=$ETCD_INITIAL_ADVERTISE_PEER_URLS --endpoints=$ETCDCTL_ENDPOINTS
 
-ETCDCTL_API=3 etcdctl member add etcd$UNIQUESID --peer-urls=$ETCD_INITIAL_ADVERTISE_PEER_URLS
 echo ETCD_INITIAL_CLUSTER_STATE=existing >> /etc/etcd/runtime_etcd.conf
+cat /etc/etcd/runtime_etcd.conf
 fi
--- roles/etcd-service/files/script/post_run.sh
+++ roles/etcd-service/files/script/post_run.sh
@@ -4,5 +4,6 @@ if [ ! -z $lockid ]
 then
   export etcd_endpoint=$( ETCDCTL_API=3 etcdctl endpoint health| grep -v "unhealthy" | head -n 1 | awk '{print $1}' )
   curl $etcd_endpoint/v3alpha/lock/unlock -XPOST -d"{\"key\":\"$lockid\" }"
+  cat /etc/etcd/runtime_etcd.conf
   rm -rf /etc/etcd/runtime_etcd.conf
 fi
