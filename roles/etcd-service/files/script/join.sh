5b34643c8e10e3f380a42dcdc5f1a3566113d5a6
樊尚享 <martinfan@yunify.com>
2017-09-30 13:44:13 +0800

    fix defect

--- roles/etcd-service/files/script/join.sh
+++ roles/etcd-service/files/script/join.sh
@@ -6,6 +6,7 @@ elif [[ $ADDING_HOST_NUM -ne 0 ]]
 then
   index=0
   unset ETCDCTL_ENDPOINTS
+
   while [ "$index" -lt $HOST_NUM ]
   do
     hostIpname=HOST_$index
@@ -22,12 +23,11 @@ then
   echo etcd_endpoint=$etcd_endpoint >> /etc/etcd/runtime_etcd.conf
   lid=$(ETCDCTL_API=3 etcdctl --endpoints=$ETCDCTL_ENDPOINTS -w fields lease grant 60 | grep '"ID"' | awk ' { print $3 } ')
   echo lockid=$(curl $etcd_endpoint:2379/v3alpha/lock/lock -XPOST -d"{\"name\":\"ZXRjZGNsdXN0ZXJsb2Nr\", \"lease\" : $lid }"|jq '.key' ) >>  /etc/etcd/runtime_etcd.conf
-
   unset ETCD_INITIAL_CLUSTER
-  ETCDCTL_API=3 etcdctl member list --endpoints=$ETCDCTL_ENDPOINTS |grep ' started' | awk -F \,  '{ gsub(/ /, "", $0);print $3"="$4 }'|{  while read node; do  if [[ -z $ETCD_INITIAL_CLUSTER ]]; then export ETCD_INITIAL_CLUSTER=$node; else export ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,$node; fi; done; echo ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,etcd$UNIQUESID=$ETCD_INITIAL_ADVERTISE_PEER_URLS >>  /etc/etcd/runtime_etcd.conf ; }
+  ETCD_INITIAL_CLUSTER=$(ETCDCTL_API=3 etcdctl member list --endpoints=http://$etcd_endpoint:2379 |grep ' started' | awk -F \,  '{ gsub(/ /, "", $0);print $3"="$4 }'|{ while read node; do  if [[ -z $ETCD_INITIAL_CLUSTER ]]; then export ETCD_INITIAL_CLUSTER=$node; else export ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,$node; fi; done; echo $ETCD_INITIAL_CLUSTER; } )
 
   ETCDCTL_API=3 etcdctl member add etcd$UNIQUESID --peer-urls=$ETCD_INITIAL_ADVERTISE_PEER_URLS --endpoints=$ETCDCTL_ENDPOINTS
-
+  echo ETCD_INITIAL_CLUSTER=$ETCD_INITIAL_CLUSTER,etcd$UNIQUESID=$ETCD_INITIAL_ADVERTISE_PEER_URLS >>  /etc/etcd/runtime_etcd.conf
   echo ETCD_INITIAL_CLUSTER_STATE=existing >> /etc/etcd/runtime_etcd.conf
   cat /etc/etcd/runtime_etcd.conf
 fi
